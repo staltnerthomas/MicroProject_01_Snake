8c7c0ac610d9a287c1bd8f05da3e7d7ee6db9799
thomasstaltner <thomas.staltner@students.fh-hagenberg.at>
2015-03-26 15:25:55 +0100

    Merged Version of Max and Thomas SnakeList is integrated...

--- app/src/main/java/at/mc/android/max/thomas/microproject_01_snake/GameView.java
+++ app/src/main/java/at/mc/android/max/thomas/microproject_01_snake/GameView.java
@@ -29,6 +29,9 @@ public class GameView extends Activity implements SurfaceHolder.Callback, View.O
     private int snakeWidth = 15;
     private int fruitWidth = snakeWidth;
     private int snakeHead = 20;
+    private SnakeList snList = new SnakeList();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +56,12 @@ public class GameView extends Activity implements SurfaceHolder.Callback, View.O
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
                 SViewWidth = width;
                 SViewHeight = height;
-                setFruit();
+                //                Canvas c = sHolder.lockCanvas();
+//                SharedPreferences sharedPrefs = getSharedPreferences(StartScreen.SHARED_PREFS, MODE_PRIVATE);
+//                c.drawColor(sharedPrefs.getInt(StartScreen.GAME_VIEW_BACKGROUND_COLOUR, 0x0));
+                snList.pushBack(getRandom(fruitWidth, SViewWidth), getRandom(fruitWidth, SViewHeight));
+                snList.pushFront(getRandom(fruitWidth, SViewWidth), getRandom(fruitWidth, SViewHeight));
+                setPixel();
             }
 
             @Override
@@ -75,7 +83,7 @@ public class GameView extends Activity implements SurfaceHolder.Callback, View.O
             @Override
             public boolean onSingleTapUp(MotionEvent e) {
                 return false;
-            }ödlkafdsölkfjasöldfjadlsöfa
+            }
 
             @Override
             public boolean onScroll(MotionEvent _e1, MotionEvent _e2, float _distanceX, float _distanceY) {
@@ -179,6 +187,84 @@ public class GameView extends Activity implements SurfaceHolder.Callback, View.O
         }
         return true;
     }
+
+
+
+    private int getRandom(int border, int max) {
+        int ret = Integer.MIN_VALUE;
+
+        do {
+            ret = (int) (Math.random() * max);
+        } while (ret < border / 2 || ret > max - border / 2);
+
+        return ret;
+    }
+
+    private int[] getRandomOld(int border) {
+        int[] ret = new int[2];
+
+        do {
+            ret[0] = (int) (Math.random() * SViewWidth);
+        } while (ret[0] < border / 2 || ret[0] > SViewWidth - border / 2);
+
+        do {
+            ret[1] = (int) (Math.random() * SViewHeight);
+        } while (ret[1] < border / 2 || ret[1] > SViewHeight - border / 2);
+
+        return ret;
+    }
+
+
+    //    private boolean first = true;
+    private void setPixel() {
+
+        if (sHolder != null) {
+            Canvas c = sHolder.lockCanvas();
+
+            SharedPreferences sharedPrefs = getSharedPreferences(StartScreen.SHARED_PREFS, MODE_PRIVATE);
+
+            c.drawColor(sharedPrefs.getInt(StartScreen.GAME_VIEW_BACKGROUND_COLOUR, 0x0));
+
+            //The fruit
+            Paint f = new Paint();
+            f.setStrokeWidth(3.0f);
+            f.setColor(sharedPrefs.getInt(StartScreen.GAME_VIEW_FRUIT_COLOUR, 0x0));
+
+            //The head
+            Paint h = new Paint();
+            h.setStrokeWidth(3.0f);
+            h.setColor(sharedPrefs.getInt(StartScreen.GAME_VIEW_SNAKE_HEAD_COLOUR, 0x0));
+
+            //The tail
+            Paint t = new Paint();
+            t.setStrokeWidth(3.0f);
+            t.setColor(sharedPrefs.getInt(StartScreen.GAME_VIEW_SNAKE_BODY_COLOUR, 0x0));
+
+
+            /**
+             * This method sets the snake and the foot
+             * The first element of the list is the head, the last is the foot
+             * Between is the tail of the snake.
+             */
+            SnakeNode n = snList.getHead();
+            for (int i = 0; i < snList.elements(); i++) {
+
+                if (i == 0) {
+                    c.drawRect(new RectF(n.getValueX() - fruitWidth / 2, n.getValueY() + fruitWidth / 2, n.getValueX() + fruitWidth / 2, n.getValueY() - fruitWidth / 2), h);
+                } else if (i == snList.elements() - 1) {
+                    c.drawRect(new RectF(n.getValueX() - fruitWidth / 2, n.getValueY() + fruitWidth / 2, n.getValueX() + fruitWidth / 2, n.getValueY() - fruitWidth / 2), f);
+                } else {
+                    c.drawRect(new RectF(n.getValueX() - fruitWidth / 2, n.getValueY() + fruitWidth / 2, n.getValueX() + fruitWidth / 2, n.getValueY() - fruitWidth / 2), t);
+                }
+
+                n = n.getNext();
+                Log.i(TAG, "draw fruit --> ");
+
+            }
+            sHolder.unlockCanvasAndPost(c);
+        }
+    }
+
 
     @Override
     public void surfaceCreated(SurfaceHolder _holder) {
